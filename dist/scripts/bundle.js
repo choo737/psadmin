a7e8c3827a841f7646684a470d9fa68af03bb4f8
Jacky Choo <choochoonchee@gmail.com>
2017-04-27 00:32:19 +0800

    added linting, installed react, react router and flux

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -9213,6 +9213,7 @@ return jQuery;
 },{}],2:[function(require,module,exports){
 $ = jQuery = require('jQuery');
 
+var test = 1;
 var App = console.log('Hello world from Browserify');
 
 module.exports = App;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 $ = jQuery = require('jQuery');
 
+var test = 1;
 var App = console.log('Hello world from Browserify');
 
 module.exports = App;
